33094a851a44e3c6485ffbcfa3c75982abf7a0fd
giovanne <giovanne.aquino.clear@gmail.com>
2023-10-03 10:40:51 -0300

    update: configure user addition and editing

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -2,17 +2,115 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import Main from '../template/Main'
 
-const headerProps = {
+const headerProps = { // define propriedades do usuario
     icon: 'users',
     title: 'Usuários',
     subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir!'
 }
 
+const baseUrl = 'http://localhost:3001/users' // define porta
+const initialState={ //define estado inicial do usuario
+    user: {name: '', email: ''},
+    list:[]
+}
+
 export default class UserCrud extends Component{
+
+    state ={...initialState}
+
+    componentWillMount() {
+        axios(baseUrl).then(resp => {
+            this.setState({ list: resp.data })
+        })
+    }
+
+    clear(){// limpa formulario
+        this.setState({user:initialState.user})// limpa usando set state somente a propriedade 'user'
+    }
+
+    save(){
+        const user = this.state.user//pega o estado do usuario
+        const method = user.id ? 'put' : 'post'// verifica se tem id(ou seja, se existe)
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl //coloca id na url
+        axios[method](url,user)// define axios(metodo put ou post)(url+id+estado do usuario)
+            .then(resp=> {
+                const list = this.getUpdatedList(resp.data)
+                this.setState({user: initialState.user, list })
+            })
+    }
+
+
+    getUpdatedList(user, add = true) {
+        const list = this.state.list.filter(u => u.id !== user.id)
+        if(add) list.unshift(user)
+        return list
+    }
+
+    updateField(event) {
+        const user = { ...this.state.user }
+        user[event.target.name] = event.target.value
+        this.setState({ user })
+    } 
+    renderForm() {
+        return (
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Nome</label>
+                            <input type="text" className="form-control"
+                                name="name"
+                                value={this.state.user.name}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o nome..." />
+                        </div>
+                    </div>
+
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>E-mail</label>
+                            <input type="text" className="form-control"
+                                name="email"
+                                value={this.state.user.email}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o e-mail..." />
+                        </div>
+                    </div>
+                </div>
+
+                <hr />
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <button className="btn btn-primary"
+                            onClick={e => this.save(e)}>
+                            Salvar
+                        </button>
+
+                        <button className="btn btn-secondary ml-2"
+                            onClick={e => this.clear(e)}>
+                            Cancelar
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    load(user) {
+        this.setState({ user })
+    }
+
+    remove(user) {
+        axios.delete(`${baseUrl}/${user.id}`).then(resp => {
+            const list = this.getUpdatedList(user, false)
+            this.setState({ list })
+        })
+    }
+
     render(){
         return(
             <Main {...headerProps}>
-                Cadastro
+                {this.renderForm()}                
             </Main>
         )
     }
